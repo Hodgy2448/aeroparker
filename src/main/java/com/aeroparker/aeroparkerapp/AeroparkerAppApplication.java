a51ed85e9cfc2a8d7d66b084280c4dcc07b3f41f
Hodgy2448 <78037871+Hodgy2448@users.noreply.github.com>
2023-11-16 12:52:19 +0000

    Initial commit

---
+++ src/main/java/com/aeroparker/aeroparkerapp/AeroparkerAppApplication.java
@@ -0,0 +1,13 @@
+package com.aeroparker.aeroparkerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroparkerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroparkerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeroparker/aeroparkerapp/AeroparkerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeroparker.aeroparkerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroparkerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
